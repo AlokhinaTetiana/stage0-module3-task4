d6dfd55ad0e3de0608e9bb78e75d3900e012cefa
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-06-25 19:25:18 +0300

    F

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a='a';
+        int r = 'r';
+        System.out.println(c+"\n"+h+"\n"+a+"\n"+r);
     }
 }
